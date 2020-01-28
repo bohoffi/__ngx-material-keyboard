db93e48408cd03e95fe720157ca37b9435887c04
Basti Hoffmann <bo.hoffi@gmail.com>
2020-01-28 18:42:16 +0100

    created project structure

---
+++ projects/ngx-material-keyboard/src/lib/ngx-material-keyboard.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-material-keyboard',
+  template: `
+    <p>
+      ngx-material-keyboard works!
+    </p>
+  `,
+  styles: []
+})
+export class NgxMaterialKeyboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/ngx-material-keyboard/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-material-keyboard
+ */
+
+export * from './lib/ngx-material-keyboard.service';
+export * from './lib/ngx-material-keyboard.component';
+export * from './lib/ngx-material-keyboard.module';
